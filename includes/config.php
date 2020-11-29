849a9d3073e1711e26088332c7daaef37c037ef6
jassKaran22 <69068199+jassKaran22@users.noreply.github.com>
2020-11-28 19:57:36 -0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,10 @@
 <?php
 
 //database configuration
-define('DB_SERVER','');
-define('DB_USER','');
+define('DB_SERVER','localhost');
+define('DB_USER','root');
 define('DB_PASS' ,'');
-define('DB_NAME', '');
+define('DB_NAME', 'shopping');
 
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
