047bc1af84cfc43cbf2339018c8e64802ba04255
jassKaran22 <69068199+jassKaran22@users.noreply.github.com>
2020-11-29 14:25:19 -0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
 
 //database configuration
-define('DB_SERVER','loca.database.windows.net');
+define('DB_SERVER','MYSQLCONNSTR_defaultConnection');
 define('DB_USER','dbroot%40loca.database.windows.net');
 define('DB_PASS' ,'Admin123');
 define('DB_NAME', 'shopping');
