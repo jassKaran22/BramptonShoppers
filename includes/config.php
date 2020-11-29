861df42e261adcd35e67e8eed39a79299c78991d
jassKaran22 <69068199+jassKaran22@users.noreply.github.com>
2020-11-29 14:19:16 -0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,17 +1,4 @@
 
-<?php //echo 'fdghfdjkghkfdjgkjdfhgkdjfhgkjfdhd';
-
-$con = mysqli_connect("capstoneservers.mysql.database.azure.com","capstoneadmin%40capstoneservers", "Admin123","shopping");
-// Check connection
-if (mysqli_connect_errno())
-{
- echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-
-
-
-
-
-//die;
+<?php 
 
 ?>
