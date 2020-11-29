f8fb7e72601f87cde5ce81e61baaf54985594e78
jassKaran22 <69068199+jassKaran22@users.noreply.github.com>
2020-11-28 23:05:52 -0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 
 <?php echo 'fdghfdjkghkfdjgkjdfhgkdjfhgkjfdhd';
 
-$con = mysqli_connect("loca.database.windows.net","azure", "Admin123","shopping");
+$con = mysqli_connect("capstoneservers.mysql.database.azure.com","capstoneadmin%40capstoneservers", "Admin123","shopping");
 // Check connection
 if (mysqli_connect_errno())
 {
