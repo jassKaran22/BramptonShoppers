42ecf7eb3ebf52bd34ebfb4659c06f40aecd5b12
jassKaran22 <69068199+jassKaran22@users.noreply.github.com>
2020-11-28 19:34:48 -0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -10,7 +10,7 @@ $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
 {
- echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ echo "Failed to connect to MySQL database hahahahah: " . mysqli_connect_error();
 }
 
 ?>
