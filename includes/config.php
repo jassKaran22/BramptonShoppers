4bd6b8cca7064fb922c38c7a00c73205b89e2292
jassKaran22 <69068199+jassKaran22@users.noreply.github.com>
2020-11-28 22:52:40 -0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,6 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try { 
-    echo 'here';
     $conn = new PDO("sqlsrv:server = tcp:loca.database.windows.net,1433; Database = shopping", "dbroot", "Admin123");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
@@ -9,10 +8,17 @@ catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
-echo 'Hello!!!!';
+
 // SQL Server Extension Sample Code:
 $connectionInfo = array("UID" => "dbroot", "pwd" => "Admin123", "Database" => "shopping", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:loca.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
+
+// Check connection
+if (mysqli_connect_errno())
+{
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
 
 ?>
