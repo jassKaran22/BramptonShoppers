9a87a444935c6574c7d2eda8651a960b06046de4
jassKaran22 <69068199+jassKaran22@users.noreply.github.com>
2020-11-28 23:02:04 -0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 
 <?php echo 'fdghfdjkghkfdjgkjdfhgkdjfhgkjfdhd';
 
-$con = mysqli_connect("loca.database.windows.net","root", "Admin123","shopping");
+$con = mysqli_connect("loca.database.windows.net","azure", "Admin123","shopping");
 // Check connection
 if (mysqli_connect_errno())
 {
