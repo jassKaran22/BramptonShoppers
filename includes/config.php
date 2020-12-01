0c6089be20c6149624fe550962211bdc4f0fad06
jassKaran22 <69068199+jassKaran22@users.noreply.github.com>
2020-12-01 16:18:32 -0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,16 +1,12 @@
-<?php
+<?php 
+    $conn = getenv("MYSQLCONNSTR_localdb"); 
 
-//database configuration
-define('DB_SERVER','loca.database.windows.net');
-define('DB_USER','dbroot%40loca.database.windows.net');
-define('DB_PASS' ,'Admin123');
-define('DB_NAME', 'shopping');
-
-$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
-// Check connection
-if (mysqli_connect_errno())
-{
- echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-
+    $conarr2 = explode(";",$conn); 
+    $conarr = array();
+    foreach($conarr2 as $key=>$value){
+        $k = substr($value,0,strpos($value,'='));
+        $conarr[$k] = substr($value,strpos($value,'=')+1);
+    }
+    
+    print_r($conarr); 
 ?>
