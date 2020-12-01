840ce77a36972137cca683d60b6e65a8d47ece96
jassKaran22 <69068199+jassKaran22@users.noreply.github.com>
2020-12-01 16:38:26 -0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,5 +1,33 @@
 <?php
-    $conn = getenv("MYSQLCONNSTR_localdb"); 
+
+/* Azure in APP MySQL */
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = '';
+$connectstr_dbpassword = '';
+
+foreach ($_SERVER as $key => $value) {
+  if (strpos($key, "MYSQLCONNSTR_") !== 0) {
+    continue;
+  }
+
+  $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+  $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+  $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+  $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+
+define('DB_NAME', $connectstr_dbname);
+define('DB_USER', $connectstr_dbusername);
+define('DB_PASSWORD', $connectstr_dbpassword);
+define('DB_HOST', $connectstr_dbhost);
+
+
+
+
+
+
+    /*$conn = getenv("MYSQLCONNSTR_localdb"); 
 
     $conarr2 = explode(";",$conn); 
     $conarr = array();
@@ -16,9 +44,9 @@
     define('DB_SERVER',$$dbserver);
     define('DB_USER',$dbUser);
     define('DB_PASS' ,$dbPaswrd);
-    define('DB_NAME', 'shopping');
+    define('DB_NAME', 'shopping');*/
 
-    $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+    $con = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
     // Check connection
     if (mysqli_connect_errno())
     {
